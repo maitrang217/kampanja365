f369f9a9d2c354324ec2f55b406207e047285c2a
Trang Nguyen <nguyenthimaitrang217@gmail.com>
2014-10-28 17:50:11 +0200

    effect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+	$('#container').masonry({
+	  itemSelector: '.item'
+	});
+	loadItems();
+
 	var wi = $(window).width();
 	var divHeight = $('.item:not(.w2)').width();
 	$('.item:not(.w2)').height(divHeight);
@@ -22,6 +27,28 @@ $(document).ready(function() {
 		}
 	});
 	
+
+	$('.item').mouseenter(function(event) {
+		$(this).children('.caption').css('padding-top', '10px').animate({height: 60}, 300);
+	});
+	$('.item').mouseleave(function(event) {
+		$(this).children('.caption').animate({height: 0}, 300).css('padding-top', '0px');
+	});
+
+
+	$(window).bind('scroll', function() {
+		parallaxScroll();
+	});
+
+});
+
+function parallaxScroll() {
+		var windowScroll = $(this).scrollTop();
+		$('.top-wrapper').css({'top': -(.5 * windowScroll) + 'px'});
+		$('.logo').css({'top': -(.5 * windowScroll) + 'px'})
+}
+
+function loadItems() {
 	$.getJSON( "js/data.json", function( data ) {
 		var items = [];
 		var captions = [];
@@ -32,21 +59,14 @@ $(document).ready(function() {
 	  });
 
 	 $('.item:not(.w2)').each(function(index) {
-	 		$(this).css({'background' : 'url("'+ items[index] +'") top left no-repeat', 'background-size':'cover'});
+	 		//$(this).css({'background' : 'url("'+ items[index] +'") top left no-repeat', 'background-size':'cover'});
+	 		$(this).append('<img src="' + items[index] + '" alt="" width="100%" height="100%">')
 	 		$(this).append('<div class="caption">' + captions[index] + '</div>');
 	 });
 	});
-	
 
-	$('.item').mouseenter(function(event) {
-		$(this).children('.caption').css('padding-top', '10px').animate({height: 60}, 300);
+	$('#container').imagesLoaded(function() {
+		var item = $('div.item')
+		$('#container').masonry('appended', item);
 	});
-	$('.item').mouseleave(function(event) {
-		$(this).children('.caption').animate({height: 0}, 300).css('padding-top', '0px');
-	});
-	
-	$('#container').masonry({
-	  itemSelector: '.item'
-	});
-
-});
+}
